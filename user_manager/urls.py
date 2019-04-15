ebc8b753ed01d1c603c7d2a3a9818989e2fd4188
Temur <tim_1998@bk.ru>
2019-04-15 16:04:29 +0300

    feat[profile]: add a functionality to change the password
    #feat #profile #pass

--- user_manager/urls.py
+++ user_manager/urls.py
@@ -3,4 +3,5 @@ from user_manager import views
 
 urlpatterns = [
     path('', views.personal, name='personal'),
+    path('change_password/', views.change_password, name='change_password')
 ]
--- user_manager/views.py
+++ user_manager/views.py
@@ -1,9 +1,12 @@
-from django.shortcuts import render
 from django.urls import reverse
 from django.http import (
     HttpResponseRedirect,
-    HttpResponse,
 )
+
+from django.contrib import messages
+from django.contrib.auth import update_session_auth_hash
+from user_manager.forms import PasswordChangeForm
+from django.shortcuts import render
 
 from django.contrib.auth import get_user_model
 User = get_user_model()
@@ -21,3 +24,24 @@ def require_authorized(function):
 def personal(request):
     user = User.objects.get(pk=request.user.id)
     return render(request, 'user_manager/account.html', {'user': user})
+
+
+@require_authorized
+def change_password(request):
+    if request.method == 'POST':
+        form = PasswordChangeForm(request.POST)
+        if form.is_valid():
+            user = User.objects.get(pk=request.user.id)
+            old_pass = form.cleaned_data['old_password']
+            new_pass = form.cleaned_data['new_password']
+            if user.check_password(old_pass):
+                user.set_password(new_pass)
+                user.save()
+                update_session_auth_hash(request, user)
+                messages.success(request, 'Your password was successfully updated!')
+                return personal(request)
+            else:
+                messages.error(request, 'Your old password is incorrect.')
+    else:
+        form = PasswordChangeForm(request.user)
+    return render(request, 'user_manager/change_password.html', {'form': form})
