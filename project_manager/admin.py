b582c8b986e5141a3a82daffa66c169cd984d58a
Temur <tim_1998@bk.ru>
2019-04-27 21:52:50 +0300

    fix[logs]: fix some issues with timelogs

--- project_manager/admin.py
+++ project_manager/admin.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
 
 from .models import (
-        Project, Task
+        Project, Task, TimeLog
 )
 
 admin.site.register([
-    Project, Task
+    Project, Task, TimeLog
 ])
